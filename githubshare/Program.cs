b5937143786a4a8970e340fb0e1991e9826e3aa0
LeeSeolHwa123 <yah2685@gmail.com>
2024-08-22 14:59:34 +0900

    Revert "Line 8부터 설화가 장난침"
    This reverts commit 8a9281f9b740a0bbbbe3e38398ca8ccef3dc3706.

--- githubshare/Program.cs
+++ githubshare/Program.cs
@@ -4,15 +4,3 @@ Console.WriteLine("hihi");
 Console.WriteLine("건우 수정함");
 Console.WriteLine("설화임");
 Console.WriteLine("졔");
-
-Console.Write("T 또는 F를 입력해보세요: ");
-string input = Console.ReadLine();
-string jje = "지혜!!";
-if (input == "T")
-{
-    Console.WriteLine(jje);
-}
-else
-{
-    Console.WriteLine($"암 낫 {jje}");
-}
